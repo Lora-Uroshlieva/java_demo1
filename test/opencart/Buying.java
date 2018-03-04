69d4594be73aeed7532939488a37412fa7065076
Larisa Uroshlieva <lora.strizhakova@gmail.com>
2018-03-04 15:53:48 +0200

    buying test was started to create

---
+++ test/opencart/Buying.java
@@ -0,0 +1,32 @@
+package opencart;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Buying {
+    private WebDriver driver;
+    private String URL = "http://opencartt.rf.gd/";
+
+    @BeforeClass
+    public  void beforeClass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testAddToCart() {
+        driver.get(URL);
+
+    }
+}
